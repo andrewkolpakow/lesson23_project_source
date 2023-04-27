dd65dfcea94f8e20f663738da864fe3a52271db4
Andrew <andrew.vk90@gmail.com>
2023-04-27 18:13:44 +0300

    Added docker-compose.yaml

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+from flask import Flask
+
 from app import create_app
 
-app = create_app()
+app: Flask = create_app()
 
 if __name__ == '__main__':
-    app.run(port=25000)
+    app.run(host='0.0.0.0', port=25000)
